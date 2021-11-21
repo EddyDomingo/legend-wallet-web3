d1d6a1e0771942601c29250c5d519603a630658d
EddyDomingo <68975969+EddyDomingo@users.noreply.github.com>
2021-11-20 22:49:49 -0600

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -8,9 +8,3 @@ const address = '0xa01468628D1b08078E0619876007A1D7A23A49e7';
 web3.eth.getBalance(address, (err, balance) =>{
     console.log('account 1 balance:', web3.utils.fromWei(balance, 'ether'))
 })
-// web3.eth.getBalance(address, (err,bal) => {balance = bal}) // this will give me wei 
-// //convert to ether
-// web3.utils.fromWei(balance, 'ether')
-// create an address
-// web3.eth.accounts.create() //this will expose private key not the best way to do this.
-// web3.eth.accounts.wallet.create(2, )
